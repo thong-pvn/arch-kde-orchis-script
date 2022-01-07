98d371d188ed4b57e3a8a730cb64a069027c4b0c
thongpv <thongpvn@gmail.com>
2022-01-07 23:14:49 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Make Your KDE Plasma Desktop A Minimal And Elegant Look with Orchis Theme
+
+Install and configure plasmoids widget
+Plasmoids widget requirements
+- Plasma ConfigSaver
+- Latte Spacer
+- Latte Separator
+- Event Calendar
+- ditto menu
---
+++ arch-kde-orchis-script.sh
@@ -0,0 +1,29 @@
+cwd=$(pwd)
+sudo pacman -Syu
+sudo pacman -S --needed git base-devel gtk-engine-murrine sassc kvantum-qt5 curl jq
+git clone https://aur.archlinux.org/yay.git
+git clone https://github.com/vinceliuice/Orchis-kde.git
+git clone https://github.com/vinceliuice/Orchis-theme.git
+git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
+git clone https://github.com/vinceliuice/Vimix-cursors.git
+git clone https://github.com/wsdfhjxc/virtual-desktop-bar.git
+cd $cwd/yay/
+makepkg -si --needed
+cd $cwd/Orchis-kde/
+./install.sh
+cd $cwd/Orchis-theme/
+./install.sh
+cd $cwd/Tela-circle-icon-theme/
+./install.sh -a
+cd $cwd/Vimix-cursors/
+./install.sh
+cd $cwd/kde-setup-02
+cp -R wallpapers/ ~/Pictures/
+mv fonts/ .fonts/
+cp -R .fonts/ ~/
+cd $cwd/Orchis-kde/sddm
+sudo ./install.sh
+cd $cwd/virtual-desktop-bar/scripts/
+./install-dependencies-arch.sh
+./install-applet.sh
+yay -S brave-bin latte-dock-git kdeconnect visual-studio-code-bin
